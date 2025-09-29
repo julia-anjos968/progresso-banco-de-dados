acb25de37d4ca4d46e4e7b699dca2d36353dbdf2
julia-anjos968 <julia.r.anjos@aluno.senai.br>
2025-09-29 11:39:35 -0300

    feat: adiciona a pasta testes

---
+++ testes/acentos.sql
@@ -0,0 +1,14 @@
+CREATE TABLE testes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+INSERT INTO testes (nome)
+VALUES
+('João'),
+('Stéfanny'),
+('Ângelo'),
+('Márcia'),
+('Letícia'),
+('Júlia'),
+('Vinícius');
